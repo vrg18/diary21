5d7a9090822d74c4d0b52b4a6854a278de012e7b
valerii.grigorev <valerii.grigorev@simbirsoft.com>
2021-03-05 12:48:50 +0400

    http replaced by https

--- lib/data/res/properties.dart
+++ lib/data/res/properties.dart
@@ -1,6 +1,6 @@
 /// Настройки лоступа к БД и пр.
 
-const String url = 'http://libera.pro';
+const String url = 'https://libera.pro';
 const String urlData = '$url:3000';
 const String urlAuth = '$url:3001';
 const String signIn = '/signin';
